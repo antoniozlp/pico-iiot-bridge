0a0dd2a41be5d9532a3f6d758d09ddbeb46b2424
antoniozlp <antonio.zalapa@gmail.com>
2025-12-30 23:26:07 -0600

    Add FreeRTOS support to the project
    - Updated CMakeLists.txt to include FreeRTOS Kernel and configuration.
    - Created FreeRTOS_Kernel_import.cmake for locating the FreeRTOS kernel.
    - Added FreeRTOS configuration files and a demo task for LED blinking in pico-iiot-bridge.c.
    - Introduced FreeRTOS-config library for better integration.

--- pico-iiot-bridge.c
+++ pico-iiot-bridge.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+#define LED_TOGGLE_RATE 100
+
+// Blink the LED on the Pico using FreeRTOS Demo Task
+void vBlinkLedDemoTask(void *pvParameters){
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    while(1){
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(LED_TOGGLE_RATE));
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(LED_TOGGLE_RATE));
+    }
+}
+
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    sleep_ms(100);
+
+    printf("Starting ...\n");
+
+    xTaskCreate(vBlinkLedDemoTask, "Blink Task", 128, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+    return 0;
 }
