dd3623857aa10c5642bd2dda6eea9b0e2d7b616d
antoniozlp <antonio.zalapa@gmail.com>
2026-01-02 22:00:19 -0600

    Update FreeRTOS configuration based on WIZnet-PICO-FREERTOS-C repo

--- lib/FreeRTOS-config/FreeRTOSConfig.h
+++ lib/FreeRTOS-config/FreeRTOSConfig.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V202107.00
+ * FreeRTOS V202111.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -28,8 +28,6 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-#include "rp2040_config.h"
-
 /*-----------------------------------------------------------
  * Application specific definitions.
  *
@@ -42,47 +40,50 @@
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
+/* Scheduler Related */
 #define configUSE_PREEMPTION                    1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE                 0
-#define configCPU_CLOCK_HZ                      133000000
-#define configTICK_RATE_HZ                      ((TickType_t)1000)
-#define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                (configSTACK_DEPTH_TYPE)256
-#define configMAX_TASK_NAME_LEN                 16
+#define configUSE_IDLE_HOOK                     0
+#define configUSE_TICK_HOOK                     0
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES                    32
+#define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 128
 #define configUSE_16_BIT_TICKS                  0
+
 #define configIDLE_SHOULD_YIELD                 1
-#define configUSE_TASK_NOTIFICATIONS            1
-#define configTASK_NOTIFICATION_ARRAY_ENTRIES   3
+
+/* Synchronization Related */
 #define configUSE_MUTEXES                       1
 #define configUSE_RECURSIVE_MUTEXES             1
+#define configUSE_APPLICATION_TASK_TAG          0
 #define configUSE_COUNTING_SEMAPHORES           1
-#define configQUEUE_REGISTRY_SIZE               10
-#define configUSE_QUEUE_SETS                    0
+#define configQUEUE_REGISTRY_SIZE               8
+#define configUSE_QUEUE_SETS                    1
 #define configUSE_TIME_SLICING                  1
 #define configUSE_NEWLIB_REENTRANT              0
-#define configENABLE_BACKWARD_COMPATIBILITY     0
+// todo need this for lwip FreeRTOS sys_arch to compile
+#define configENABLE_BACKWARD_COMPATIBILITY     1
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
-#define configSTACK_DEPTH_TYPE                  uint32_t
+
+/* System */
+#define configSTACK_DEPTH_TYPE                  uint16_t
 #define configMESSAGE_BUFFER_LENGTH_TYPE        size_t
 
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   (128 * 1024)
+#define configTOTAL_HEAP_SIZE                   (96*1024)
 #define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
-#define configUSE_IDLE_HOOK                     0
-#define configUSE_TICK_HOOK                     0
 #define configCHECK_FOR_STACK_OVERFLOW          0
 #define configUSE_MALLOC_FAILED_HOOK            0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
 
 /* Run time and task stats gathering related definitions. */
 #define configGENERATE_RUN_TIME_STATS           0
-#define configUSE_TRACE_FACILITY                1
-#define configUSE_STATS_FORMATTING_FUNCTIONS    1
+#define configUSE_TRACE_FACILITY                0
+#define configUSE_STATS_FORMATTING_FUNCTIONS    0
 
 /* Co-routine related definitions. */
 #define configUSE_CO_ROUTINES                   0
@@ -90,12 +91,9 @@
 
 /* Software timer related definitions. */
 #define configUSE_TIMERS                        1
-#define configTIMER_TASK_PRIORITY               (configMAX_PRIORITIES - 1)
+#define configTIMER_TASK_PRIORITY               ( configMAX_PRIORITIES - 1 )
 #define configTIMER_QUEUE_LENGTH                10
-#define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
-
-#define configCOMMAND_INT_MAX_OUTPUT_SIZE       1024
-#define configSTATS_BUFFER_MAX_LENGTH           configCOMMAND_INT_MAX_OUTPUT_SIZE
+#define configTIMER_TASK_STACK_DEPTH            1024
 
 /* Interrupt nesting behaviour configuration. */
 /*
@@ -103,37 +101,53 @@
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY    [dependent on processor and application]
 #define configMAX_API_CALL_INTERRUPT_PRIORITY   [dependent on processor and application]
 */
+/* RP2350 (Cortex-M33) has 3 priority bits */
+#ifndef __NVIC_PRIO_BITS
+#define __NVIC_PRIO_BITS                        3
+#endif
+#define configKERNEL_INTERRUPT_PRIORITY         ( 3 << (8 - __NVIC_PRIO_BITS) )
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    ( 1 << (8 - __NVIC_PRIO_BITS) )
 
-/* Platform-specific ARM configuration for RP2350 */
-#if defined(PICO_RP2350) && !defined(PICO_RISCV)
-    /* Required for RP2350 ARM (Armv8-M) - only one privilege level in secure state */
-    #define configENABLE_MPU                        0
-    #define configENABLE_TRUSTZONE                  0
-    #define configRUN_FREERTOS_SECURE_ONLY          1
-    
-    /* Enable FPU support for RP2350 (recommended for floating point operations) */
-    #define configENABLE_FPU                        1
-    
-    /* Required interrupt priority configuration for RP2350 */
-    #define configMAX_SYSCALL_INTERRUPT_PRIORITY    16
+/* FPU configuration for RP2350 - CRITICAL for floating point operations in tasks */
+#if (DEVICE_BOARD_NAME == W5100S_EVB_PICO2 || DEVICE_BOARD_NAME == W5500_EVB_PICO2 || \
+     DEVICE_BOARD_NAME == W6100_EVB_PICO2 || DEVICE_BOARD_NAME == W6300_EVB_PICO2)
+#define configENABLE_FPU                        1
+#define configENABLE_TRUSTZONE                  0
+#define configRUN_FREERTOS_SECURE_ONLY          1
+#define configENABLE_MPU                        0
 #endif
 
+#ifdef SIO_IRQ_FLAG
+#define SIO_IRQ_PROC0 15
+#endif
+/* Use build-time definition if available, otherwise disable SMP */
+#ifndef FREE_RTOS_KERNEL_SMP
+#define FREE_RTOS_KERNEL_SMP 0
+#endif
+#if FREE_RTOS_KERNEL_SMP // set by the RP2040 SMP port of FreeRTOS
 /* SMP port only */
-#define configNUMBER_OF_CORES                   2
+#define configNUM_CORES                         2
+#define configNUMBER_OF_CORES                   configNUM_CORES
 #define configTICK_CORE                         0
 #define configRUN_MULTIPLE_PRIORITIES           1
 #define configUSE_CORE_AFFINITY                 1
+#define configUSE_MINIMAL_IDLE_HOOK             0
 #define configUSE_PASSIVE_IDLE_HOOK             0
+#endif
 
-/* RP2040/RP2350 specific */
+#define configUSE_TIME_SLICING 1
+
+
+/* RP2040 specific */
 #define configSUPPORT_PICO_SYNC_INTEROP         1
 #define configSUPPORT_PICO_TIME_INTEROP         1
 
-/* Define to trap errors during development. */
 #include <assert.h>
+/* Define to trap errors during development. */
 #define configASSERT(x)                         assert(x)
 
-/* Optional functions - most linkers will remove unused functions anyway. */
+/* Set the following definitions to 1 to include the API function, or zero
+to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet                1
 #define INCLUDE_uxTaskPriorityGet               1
 #define INCLUDE_vTaskDelete                     1
@@ -150,6 +164,13 @@
 #define INCLUDE_xTaskAbortDelay                 1
 #define INCLUDE_xTaskGetHandle                  1
 #define INCLUDE_xTaskResumeFromISR              1
+#define INCLUDE_xQueueGetMutexHolder            1
+
+#define configENABLE_MPU                        0
+#define configENABLE_TRUSTZONE                  0
+#define configRUN_FREERTOS_SECURE_ONLY          1
+
+/* A header file that defines trace macro can be included here. */
 
 #endif /* FREERTOS_CONFIG_H */
 
